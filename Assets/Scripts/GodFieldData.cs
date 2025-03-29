29e4a14e169691f8f999ba17f2fb9ac00f1d539e
gantamachan <skyblueappleorange@gmail.com>
2025-03-29 21:16:51 +0900

    正解表示機能　プレハブ作成済み

--- Assets/Scripts/GodFieldData.cs
+++ Assets/Scripts/GodFieldData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class koregamietetaraOK : MonoBehaviour
+public class GodFieldData : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
